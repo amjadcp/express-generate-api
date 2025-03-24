aca1cb71491a4a530b8434c3c46d774e75a5bd2f
amjadcp <amjadcp03@gmail.com>
2025-03-24 20:40:31 +0530

    chore: remove unused files and configurations from js and ts templates

--- template/ts/src/middleware/notFound.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-const notFound = (req: Request, res: Response): Response =>
-  res.status(404).send("Route does not exist :)");
-
-export default notFound;
